6745a528d317187c03c85f0b5a5b0f6a15053c79
zhangliang7190 <zl7190@126.com>
2023-07-17 20:27:49 +0800

    update migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,24 +1,24 @@
-const { mainnet, nile } = require('../scripts/config.js');
 var router = artifacts.require('./SmartExchangeRouter.sol');
 
+var old3pool = '0x332124D9aCCbCd2FFD3Be081Cfb36E959f5969c6';
+var oldusdcpool = '0x4100000000000000000000000000000000000000';
+var routerV2 = '0x332124D9aCCbCd2FFD3Be081Cfb36E959f5969c6';
+var routerV1 = '0x332124D9aCCbCd2FFD3Be081Cfb36E959f5969c6';
+var routerV3 = '0x332124D9aCCbCd2FFD3Be081Cfb36E959f5969c6';
+var usdtToken = '0x332124D9aCCbCd2FFD3Be081Cfb36E959f5969c6';
+var usdjToken = '0x332124D9aCCbCd2FFD3Be081Cfb36E959f5969c6';
+var tusdToken = '0x332124D9aCCbCd2FFD3Be081Cfb36E959f5969c6';
+var wtrxToken = '0x332124D9aCCbCd2FFD3Be081Cfb36E959f5969c6';
+var psmUsddToken = '0x332124D9aCCbCd2FFD3Be081Cfb36E959f5969c6';
+
 module.exports = function (deployer, network) {
-  if (network == 'nile') {
-    deployer.deploy(
-      router,
-      nile.routerV2,
-      nile.routerV1,
-      nile.psmUsddToken,
-      nile.routerV3,
-      nile.wtrxToken
-    );
-  } else if (network == 'mainnet') {
-    deployer.deploy(
-      router,
-      mainnet.routerV2,
-      mainnet.routerV1,
-      mainnet.psmUsddToken,
-      mainnet.routerV3,
-      mainnet.wtrxToken
-    );
-  }
+
+  deployer.deploy(
+    router,
+    routerV2,
+    routerV1,
+    psmUsddToken,
+    routerV3,
+    wtrxToken
+  );
 };
